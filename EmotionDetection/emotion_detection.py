f0f82d6b6baf9af214e0a039cac99dc4c9887904
0xAmritv <amritvkhantwal@outlook.com>
2026-09-09 05:32:43 -0400

    added error handling for status code 400, returns None for all values in emotion key-value pair

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,19 @@ def emotion_detector(text_to_analyse):
     Input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url,headers=Headers,json=Input_json)
     formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+
+
+
     emotions = formatted_response['emotionPredictions'][0]['emotion']
 
     anger_score = emotions['anger']
